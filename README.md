a49c3cc977cebcd15de9032f54b567c65b2f4d5c
674656681@qq.com <674656681@qq.com>
2017-06-05 18:08:50 +0800

    6.5 Signed-off-by: 674656681@qq.com <674656681@qq.com>

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 小程序-->目录
     "pages/module/index/index" --> 首页 --> 引导页
-    "pages/module/diy/index" --> diy页面
     "pages/module/goods_list/index" --> 商品列表
     "pages/module/goods_details/index" --> 商品详情
     "pages/module/shopping_cart/index" --> 购物车
--- pages/module/member/index.js
+++ pages/module/member/index.js
@@ -5,62 +5,89 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    static: [{
+      name: "待付款",
+      number: "0",
+      url: ""
+    }, {
+      name: "待发货",
+      number: "1",
+      url: ""
+    }, {
+      name: "待收货",
+      number: "2",
+      url: ""
+    }, {
+      name: "待评价",
+      number: "3",
+      url: ""
+    }, {
+      name: "待换货",
+      number: "4",
+      url: ""
+    }],
+    check:[
+      {name:"话题推广",img:"../../assets/images/08.png",url:""},
+      {name:"话题推广",img:"../../assets/images/08.png",url:""},
+      {name:"话题推广",img:"../../assets/images/08.png",url:""},
+      {name:"话题推广",img:"../../assets/images/08.png",url:""},
+      {name:"话题推广",img:"../../assets/images/08.png",url:""},
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
